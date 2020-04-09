294726b5db81da894b9cc0314b2861f865747064
papergirl <aisa.mijeno@dlsl.edu.ph>
2020-04-10 00:35:54 +0800

    pollutantmean.R

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,17 +1,25 @@
-pollutantmean<-function(directory,pollutant,id=1:332){
-  #create a list of files
-  filesD<-list.files(directory,full.names = TRUE)
-  #create an empty data frame
-  dat <- data.frame()
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
   
-  #loop through the list of files until id is found
-  for(i in id){
-    #read in the file
-    temp<- read.csv(filesD[i],header=TRUE)
-    #add files to the main data frame
-    dat<-rbind(dat,temp)
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of  the pollutant for which we will calcultate the
+  ## mean; either "sulfate" or "nitrate"
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
+  ## NOTE: Do not round the result
+  means <- c()
+  
+  for(monitor in id){
+    path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+    monitor_data <- read.csv(path)
+    interested_data <- monitor_data[pollutant]
+    means <- c(means, interested_data[!is.na(interested_data)])
   }
-  #find the mean of the pollutant, make sure you remove NA values
-  return(mean(dat[,pollutant],na.rm = TRUE))
   
+  mean(means)
 }
